dbb8da256b6fcb60e339fe276f328bfe6569d7e4
Weerawi <weerawikramatharindu@gmail.com>
2024-02-25 10:50:37 +0530

    product direct and productdetails display component created

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrumms/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+    const all_products = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_products.find((e) => e.id === Number(productId));
+
+    
+
+    return (
+        <div className='mx-auto max-w-[1280px] '>
+            <Breadcrum product={product} />
+            <ProductDisplay product={product} />
+            <DescriptionBox/>
+            <RelatedProducts/>
+        </div>
+    )
 }
 
-export default Product
+export default Product;
