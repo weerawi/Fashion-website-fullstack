5d7728aeb0e8d332b043f2b324f3356025196880
Weerawi <weerawikramatharindu@gmail.com>
2024-02-26 17:39:39 +0530

    upload images endpoint is working

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,32 @@ app.get("/", (req, res) => {
     res.send("Welcome to Fashion Website Express");
 });
 
+
+// Image storage engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images', //path.join(__dirname, 'upload', 'images'), // Use absolute path instead of this  destination: './upload/images',
+    filename:  (req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({storage:storage})
+
+
+
+// creating upload endpoint for images
+
+app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`}
+    );
+});
+
+
+
 app.listen(port, (error) => {
     if(!error){
         console.log(`Server is running at port ${port}`);
@@ -32,4 +58,3 @@ app.listen(port, (error) => {
 
 
 
- 
