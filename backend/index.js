4619641349bf7dbdf3720477435506c6e01e75bf
Weerawi <weerawikramatharindu@gmail.com>
2024-03-01 18:46:04 +0530

    creating endpoint for populat in women in backend

--- backend/index.js
+++ backend/index.js
@@ -248,6 +248,15 @@ app.get('/newcollections',async(req,res)=>{
 })
 
 
+//creating endpoint for popular in women section
+app.get('/popularinwoman',async(req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched ");
+    res.send(popular_in_women);
+})
+
+
 app.listen(port, (error) => {
     if(!error){
         console.log(`Server is running at port ${port}`);
