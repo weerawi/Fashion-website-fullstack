74c737718457b19d9286bdd7cfccb9b720e27da5
Weerawi <weerawikramatharindu@gmail.com>
2024-03-03 07:48:05 +0530

    created the delete the cartItem endpoint in backend

--- backend/index.js
+++ backend/index.js
@@ -311,6 +311,19 @@ app.post('/getcart',fetchUser,async(req,res)=>{
 })
 
 
+
+//creating endpoint to delete cartItems
+app.post('/deletefromcart',fetchUser,async(req,res)=>{
+    console.log("Deleted",req.body.itemId);
+
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId]=0;
+    await  Users.findOneAndUpdate({_id : req.user.id},{cartData:userData.cartData} );
+    res.send("Deleted");
+
+})
+
 app.listen(port, (error) => {
     if(!error){
         console.log(`Server is running at port ${port}`);
