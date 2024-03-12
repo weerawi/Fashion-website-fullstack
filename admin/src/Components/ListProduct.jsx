de7350a867f983c5c639191d722f879a9b45c152
Weerawi <weerawikramatharindu@gmail.com>
2024-03-12 21:19:06 +0530

    listproduct also fetching items sizes from db that added

--- admin/src/Components/ListProduct.jsx
+++ admin/src/Components/ListProduct.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import remove_icon from '../assets/cross_icon.png';
- 
+
 
 const ListProduct = () => {
 
@@ -34,29 +34,44 @@ const ListProduct = () => {
 
 
   return (
-    <div className="flex flex-col items-center w-[80%]"> 
+    <div className="flex flex-col items-center w-[80%] mx-auto"> 
+
         <h1 className="text-3xl font-bold p-5">All Product List</h1>
-        <div className=" grid grid-cols-8 font-semibold gap-1 md:gap-2 lg:gap-10 py-1  ">
+        <div className=" grid grid-cols-9 font-semibold gap-1 md:gap-2 lg:gap-10 py-1 my-5 ">
             <p className='flex justify-center'>Products</p>
             <p className='col-start-2 col-span-3 flex justify-center '>Title</p>
             <p className='flex justify-center' >Old Price</p>
             <p className='flex justify-center '>New Price</p>
             <p className='flex justify-center '>Category</p>
+            <p className='flex justify-center '>Size</p>
             <p className='flex justify-center'>Remove</p>
         </div>
 
         <div className="my-5 py-5  bg-gray-50 overflow-y-scroll h-1/3">
 
+        
+
 
             {allProducts.map((product,i) => {
                 
-                return  <div key={i} className="   items-center border-b-2 gap-1 md:gap-2 lg:gap-10
-                border-gray-400 py-2 grid grid-cols-8  ">
+                return  <div key={i} className="   items-center border-b-2  grid grid-cols-9 gap-1 md:gap-2 lg:gap-10 py-1 
+                border-gray-400 ">
                             <img src={product.image} width={80} height={80} alt="" className=" mx-auto" />
                             <p className='flex justify-center col-start-2 col-span-3 '>{product.name}</p>
                             <p className='flex justify-center '>${product.old_price}</p>
                             <p className='flex justify-center '>${product.new_price}</p>
                             <p className='flex justify -center'>${product.category}</p>
+                            <p className='flex justify-center'>
+                                <div className="flex">
+                                    {Object.entries(product.size).map(([size, quantity]) => {
+                                        if (quantity > 0) {
+                                            return <span key={size}> {size}: {quantity}  </span>;
+                                        }
+                                        return null; // Return null for sizes with quantity <= 0
+                                    })}
+                                </div>
+                            </p>
+
                             <img className="cursor-pointer  mx-auto " width={10} height={10} src={remove_icon} alt="" onClick={() => {remove_product(product.id)}} />
                         </div>
                 
